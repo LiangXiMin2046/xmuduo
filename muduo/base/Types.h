396cf0c5b903d2b52282d9640b9e576a24a961b4
LiangXiMin2046 <1045886584@qq.com>
2020-06-20 19:05:33 +0800

    type trans

---
+++ muduo/base/Types.h
@@ -0,0 +1,36 @@
+#ifndef _MUDUO_BASE_TYPES_H
+#define _MUDUO_BASE_TYPES_H
+
+#include<stdint.h>
+#ifdef MUDUO_STD_STRING
+#include<string>
+#else
+#include<ext/vstring.h>
+#include<ext/vstring_fwd.h>
+#endif
+
+namespace muduo{
+#ifdef MUDUO_STD_STRING
+using std::string;
+#else
+typedef __gnu_cxx::__sso_string string;
+#endif
+
+template<typename To,typename From>
+inline To implicit_cast(const &F){
+	return F;
+}
+
+template<typename To,typename From>
+inline To down_cast(From* f){
+  if (false) {
+    implicit_cast<From*, To>(0);
+  }
+#if !defined(NDEBUG) && !defined(GOOGLE_PROTOBUF_NO_RTTI)
+  assert(f == NULL || dynamic_cast<To>(f) != NULL);  // RTTI: debug mode only
+#endif
+  return static_cast<To>(f);	
+}
+ 
+}
+#endif
