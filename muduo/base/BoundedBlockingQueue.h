4352910db3481fc98ff23949c146fbbccca5c1d0
LiangXiMin2046 <1045886584@qq.com>
2020-07-02 16:09:01 +0800

    Update BoundedBlockingQueue.h

--- muduo/base/BoundedBlockingQueue.h
+++ muduo/base/BoundedBlockingQueue.h
@@ -1,8 +1,8 @@
 #ifndef MUDUO_BASE_BOUNDBLOCKINGQUEUE_H
 #define MUDUO_BASE_BOUNDBLOCKINGQUEUE_H
 
-#include "/muduoday/day6/muduo/base/Mutex.h"
-#include "/muduoday/day6/muduo/base/Condition.h"
+#include "/xmuduo/muduo/base/Mutex.h"
+#include "/xmuduo/muduo/base/Condition.h"
 
 #include<deque>
 #include<boost/noncopyable.hpp>
