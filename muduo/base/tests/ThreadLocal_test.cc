bf9264a7b9ed03380f467cfbc9d7d7bf730f2e09
LiangXiMin2046 <1045886584@qq.com>
2020-07-05 18:11:57 +0800

    ThreadLocal test

---
+++ muduo/base/tests/ThreadLocal_test.cc
@@ -0,0 +1,55 @@
+#include "/muduoday/day9/muduo/base/ThreadLocal.h"
+#include "/muduoday/day9/muduo/base/CurrentThread.h"
+#include "/muduoday/day9/muduo/base/Thread.h"
+
+#include<unistd.h>
+#include<string>
+#include<stdio.h>
+
+class Model
+{
+public:
+	Model()
+	  :name_("original name")
+	{
+	}
+	
+	void setName(const std::string& name)
+	{
+		name_ = name;	
+	}
+
+	std::string& getName()
+	{
+		return name_;
+	}
+
+private:
+	std::string name_;
+};
+
+muduo::ThreadLocal<Model> m1;
+muduo::ThreadLocal<Model> m2;
+
+void func()
+{
+	printf("in another thread,tid:%d,m1's name:%s,m2's name:%s\n",
+		muduo::CurrentThread::tid(),m1.value().getName().c_str(),m2.value().getName().c_str());
+	m1.value().setName("thread name m1");
+	m2.value().setName("thread name m2");
+	printf("name changed in another thread,tid:%d,m1's name:%s,m2's name:%s\n",
+		muduo::CurrentThread::tid(),m1.value().getName().c_str(),m2.value().getName().c_str());
+	sleep(1);	
+}
+
+int main(void)
+{
+	printf("in main thread,tid:%d,m1's name:%s,m2's name:%s\n",
+		muduo::CurrentThread::tid(),m1.value().getName().c_str(),m2.value().getName().c_str());
+	muduo::Thread t1(func,"thread1");
+	t1.start();
+	t1.join();
+	printf("in main thread,tid:%d,m1's name:%s,m2's name:%s\n",
+		muduo::CurrentThread::tid(),m1.value().getName().c_str(),m2.value().getName().c_str());
+	return 0;
+}
