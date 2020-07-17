9ee4d8ba7559985d63072735dcb0d6b6ab4c2e03
LiangXiMin2046 <1045886584@qq.com>
2020-07-17 20:44:18 +0800

    update

--- muduo/net/EventLoop.cc
+++ muduo/net/EventLoop.cc
@@ -1,6 +1,9 @@
 #include "/xmuduo/muduo/net/EventLoop.h"
 #include "/xmuduo/muduo/base/Logging.h"
-#include <poll.h>
+#include "/xmuduo/muduo/net/Poller.h"
+#include "/xmuduo/muduo/net/Channel.h"
+
+//#include <poll.h>
 
 using namespace muduo;
 using namespace muduo::net;
@@ -8,6 +11,8 @@ using namespace muduo::net;
 namespace
 {
 __thread EventLoop* t_loopInThisThread = 0;
+
+const int kPollTimeMs = 10000;
 }
 
 EventLoop* EventLoop::getEventLoopOfCurrentThread()
@@ -16,8 +21,13 @@ EventLoop* EventLoop::getEventLoopOfCurrentThread()
 }
 
 EventLoop::EventLoop()
-	:	looping_(false),
-		threadId_(CurrentThread::tid())
+	:	
+		looping_(false),
+		quit_(false),
+		eventHandling_(false),
+		poller_(Poller::newDefaultPoller(this)),
+		threadId_(CurrentThread::tid()),
+		currentActiveChannel_(NULL)
 {
 	LOG_TRACE<< "EventLoop created" << this << "in thread" << threadId_;
 	if(t_loopInThisThread)
@@ -43,14 +53,70 @@ void EventLoop::loop()
 	looping_ = true;
 	LOG_TRACE << "EventLoop" << this << "start looping";
 
-	::poll(NULL,0,5*1000);
-	
+	//::poll(NULL,0,5*1000);
+	while(!quit_)
+	{
+		activeChannels_.clear();
+		pollReturnTime_ = poller_ -> poll(kPollTimeMs,&activeChannels_);
+		if(Logger::logLevel() <= Logger::TRACE)
+		{
+			printActiveChannels();
+		}
+
+		eventHandling_ = true;
+		for(ChannelList::iterator it = activeChannels_.begin();
+				it != activeChannels_.end(); ++it)
+		{
+			currentActiveChannel_ = *it;
+			currentActiveChannel_ -> handleEvent(pollReturnTime_);
+		}
+		currentActiveChannel_ = NULL;
+		eventHandling_ = false;
+	}	
 	LOG_TRACE << "EventLoop" << this << "stop looping";
 	looping_ = false;
 }
 
+void EventLoop::quit()
+{
+	quit_ = true;
+	if(!isInLoopThread())
+	{
+		
+	}
+}
+
+void EventLoop::updateChannel(Channel* channel)
+{
+	assert(channel->ownerLoop() == this);
+	assertInLoopThread();
+	poller_ -> updateChannel(channel);
+}
+
+void EventLoop::removeChannel(Channel* channel)
+{
+	assert(channel -> ownerLoop() == this);
+	assertInLoopThread();
+	if(eventHandling_)
+	{
+		assert(currentActiveChannel_ == channel ||
+				std::find(activeChannels_.begin(),activeChannels_.end(),channel) 
+					== activeChannels_.end());
+	}
+	poller_ -> removeChannel(channel);
+}
 void EventLoop::abortNotInLoopThread()
 {
 	LOG_FATAL << "EventLoop::abortNotIntLoopThread-EventLoop"<<this<<" was created in threadId_ = " << threadId_
 				<< ",current thread id = " << CurrentThread::tid();
+}
+
+void EventLoop::printActiveChannels() const
+{
+	for(ChannelList::const_iterator it = activeChannels_.begin();
+			it != activeChannels_.end(); ++it)
+	{
+		const Channel* ch = *it;
+		LOG_TRACE << "{" << ch -> reventsToString() << "} ";
+	}
 }
--- muduo/net/EventLoop.h
+++ muduo/net/EventLoop.h
@@ -1,15 +1,20 @@
 #ifndef MUDUO_NET_EVENTLOOP_H
 #define MUDUO_NET_EVENTLOOP_H
 
+#include <vector>
 #include <boost/noncopyable.hpp>
+#include <boost/scoped_ptr.hpp>
 
 #include "/xmuduo/muduo/base/CurrentThread.h"
 #include "/xmuduo/muduo/base/Thread.h"
+#include "/xmuduo/muduo/base/Timestamp.h"
 
 namespace muduo
 {
 namespace net
 {
+class Channel;
+class Poller;
 //Reactor,at most one per thread.
 class EventLoop : boost::noncopyable
 {
@@ -18,7 +23,14 @@ public:
 	~EventLoop();
 	
 	void loop();
-	
+	void quit();
+	//
+	//Time when poll returns,usually means data arrival
+	//
+	Timestamp pollReturnTime() const {return pollReturnTime_;}
+
+	void updateChannel(Channel* channel);
+	void removeChannel(Channel* channel);
 	void assertInLoopThread()
 	{
 		if(!isInLoopThread())
@@ -31,8 +43,19 @@ public:
 	static EventLoop* getEventLoopOfCurrentThread();
 private:
 	void abortNotInLoopThread();
+	
+	void printActiveChannels() const;
+	
+	typedef std::vector<Channel*>ChannelList;
+
 	bool looping_;
+	bool quit_;
+	bool eventHandling_;
 	const pid_t threadId_;
+	Timestamp pollReturnTime_;
+	boost::scoped_ptr<Poller> poller_;
+	ChannelList activeChannels_;
+	Channel* currentActiveChannel_;
 };
 
 }
